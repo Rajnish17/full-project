a3c256aaa4a339dd271c198e39492242e4245a23
Rajnish <RajnishAryan12@gmail.com>
2023-07-23 13:04:27 +0530

    get and post route done

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,14 +1,56 @@
-const User =require("../models/UserSchema");
-const express =require("express");
-const router =express.Router();
+const User = require("../models/UserSchema");
+const express = require("express");
+const router = express.Router();
+
+
+//post user
+
+router.post("/register", async(req,res) => {
+
+    const { name, email, contact } = req.body
+
+    try {
+
+        if (!name || !email || !contact) {
+           return res.status(401).json({error:"please fill all details"})
+        }
+
+        const newUser = new User({
+            name,
+            email,
+            contact
+        });
+
+        await newUser.save();
+
+        res.status(200).json({
+            message:"success",
+            data:newUser
+        })
 
 
 
+    } catch (error) {
+        res.status(400).json(error)
+    }
 
-router.get("/register",(req,res)=>{
 
-    res.send("hello");
 })
 
 
-module.exports= router
+//get user data
+
+router.get("/show", async (req, res) => {
+    try {
+        const data = await User.find();
+        res.status(200).json({
+            message: "sucess",
+            data: data
+        })
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+
+module.exports = router
