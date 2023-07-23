bd76151b9e9b4a1334807933dcacd8355febac3f
Rajnish <RajnishAryan12@gmail.com>
2023-07-23 11:57:34 +0530

    userSchema created

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const User = mongoose.Schema;
+
+const UserSchema = new User({
+    name: { type: String},
+    email: { type: String},
+    contact:{type:Number},
+    
+
+  }, {
+    timestamps: true,
+  });
+  
+  module.exports =mongoose.model('User', UserSchema);
